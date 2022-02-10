d218ef472a55f26592474e0d97b0f55f57593bb5
Eslam Mounir AL Sadat <alsadateslammounir@hotmail.com>
2022-02-10 21:10:03 +0200

    Add project files.

---
+++ Logical_Operators_Example/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Operators_Example
+{
+    public class Program
+    {
+            static void Main(string[] args)
+            {
+                // ----- Start Example ----- \\
+
+                int X = 15;
+                int Y = 10;
+                bool a = true, Result;
+
+                // AND operator
+                Result = (X <= Y) && (X > 10); 
+                Console.WriteLine("AND Operator: " + Result); // Output ==> False
+
+                // ------------------ \\
+
+                // OR operator
+                Result = (X >= Y) || (X < 5); 
+                Console.WriteLine("OR Operator: " + Result); // Output ==> True
+
+                // ------------------ \\
+
+                // NOT operator
+                Result = !a; 
+                Console.WriteLine("NOT Operator: " + Result); // Output ==> False
+
+                // ----- Start Example ----- \\
+            }
+    }
+}
